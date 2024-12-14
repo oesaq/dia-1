d5bb6641249fc0798f4edd7eb8d6e77c39e899e8
Isaque Oliveira <proisaqueoliveira@gmail.com>
2024-12-13 22:25:57 -0300

    adicionando dia 7

---
+++ dia-7/index.js
@@ -0,0 +1,11 @@
+let condition = true;
+
+while (condition) {
+    let idade = Number(prompt('Insira sua idade: '));
+    if (idade < 18) {
+        console.log('Você é menor de idade')
+        condition = false;
+    }
+
+}
+console.log('Acabou o while');
