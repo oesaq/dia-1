8e8f113c9b66670773ffd8e07a1c9464c37ca259
Isaque Oliveira <proisaqueoliveira@gmail.com>
2024-12-13 21:03:26 -0300

    adicionando dia 5 e complementos do dia 3

--- dia-3/index.js
+++ dia-3/index.js
@@ -1,4 +1,11 @@
+/*
 let tipoTexto = "2";
 let tipoNumber = 2;
 let tipoBool = true;
-let tipoBoolTxt = "false";
+let tipoBoolTxt = "false";*/
+
+// let e const
+
+let nome = 'Isaque Oliveira';
+
+console.log(nome);
---
+++ dia-5/index.js
@@ -0,0 +1,19 @@
+let idade = Number(prompt('Insira sua idade: '));
+
+switch (idade) {
+    case 10:
+        console.log('Você tem 10 anos');
+        break;
+    
+    case 15:
+        console.log('Você tem 15 anos');
+        break;
+
+    case 20:
+        console.log('Você tem 20 anos');
+        break;
+
+    default:
+        console.log('default');
+        break;
+}
