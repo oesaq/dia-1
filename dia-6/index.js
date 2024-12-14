c1e27fadd8fc59923f4546c25a753f0ceccf1cd2
Isaque Oliveira <proisaqueoliveira@gmail.com>
2024-12-13 22:02:13 -0300

    adicionando o dia 6

---
+++ dia-6/index.js
@@ -0,0 +1,23 @@
+
+let tabuada = Number(prompt('Escolha a tabuada que deseja: '));
+/*
+console.log(tabuada + " x 0 = " + tabuada * 0);
+console.log(tabuada + " x 1 = " + tabuada * 1);
+console.log(tabuada + " x 2 = " + tabuada * 2);
+console.log(tabuada + " x 3 = " + tabuada * 3);
+console.log(tabuada + " x 4 = " + tabuada * 4);
+console.log(tabuada + " x 5 = " + tabuada * 5);
+console.log(tabuada + " x 6 = " + tabuada * 6);
+console.log(tabuada + " x 7 = " + tabuada * 7);
+console.log(tabuada + " x 8 = " + tabuada * 8);
+console.log(tabuada + " x 9 = " + tabuada * 9);
+console.log(tabuada + " x 10 = " + tabuada * 10);
+
+*/
+
+for (let contador = 0; contador <= 10; contador++) {
+    console.log(tabuada + " x " + contador + " = " + tabuada * contador);
+    
+}
+
+
